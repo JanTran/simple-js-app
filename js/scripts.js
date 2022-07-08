e8c21b2ac76f034fe1626fc78b67e25796ee28df
Brendon Choi <choi.brendon2001@gmail.com>
2022-07-08 16:00:32 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,15 @@ scripts.js
 alert('Hello world');
 var favoriteFood = 'Pizza';
 document.write(favoriteFood);
+var pokemonList = [];
+var type = ["grass","poison","fire","water"];
+let pokemonList[0] {
+  name = "bulbasaur";
+  height = 7;
+  type = type[0];
+}
+let pokemonList[1] {
+  name = "squirtle";
+  height = 5;
+  type = type[3];
+}
