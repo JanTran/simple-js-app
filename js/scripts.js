5195b7366f228c71efab2249283aeedd6728dc6b
Brendon Choi <choi.brendon2001@gmail.com>
2022-07-14 15:17:10 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
+alert('Hello world');
+let pokemonList = [
+    {
+        name: 'Squirtle',
+        height: 1.1,
+        types: [
+            'WATER'
+        ]
+    },
+    {   
+        name: 'Pikachu',
+        height: 0.4,
+        types: [
+            'ELECTRIC'
+        ]
+
+    },
+    {
+        name: 'Bulbasaur',
+        height: 0.5,
+        types: [
+            'GRASS', 
+            'POISON'
+        ]
+];
+pokemonList.forEach(function(pokemon)) {
+    console.log("Name: " + pokemon.name + ", type: " + pokemon.type + ", height: " + pokemon.height);
+}
 let pokemonRepository = (function () {
   let pokemonList = []; // empty array
 
