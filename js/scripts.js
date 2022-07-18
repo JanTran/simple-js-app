d63fa3092ee7b9897f8206851344e13e4b81fdb3
Brendon Choi <choi.brendon2001@gmail.com>
2022-07-18 14:56:34 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,57 @@
 let pokemonRepository = (function () {
- let pokemonList = [
-    {
-        name: 'Squirtle',
-        height: 1.1,
-        types: [
-            'WATER'
-        ]
-    },
-    {   
-        name: 'Pikachu',
-        height: 0.4,
-        types: [
-            'ELECTRIC'
-        ]
-
-    },
-    {
-        name: 'Bulbasaur',
-        height: 0.5,
-        types: [
-            'GRASS', 
-            'POISON'
-        ]
-    } 
-    ];
-  return {
-    add: function(pokemon) {
-      pokemonList.push(pokemon);
-    },
-    getAll: function() {
-      return pokemonList;
-    },
-    addListItem: function(pokemon) {
-      let pokemonList = document.querySelector(".pokemon-list");
-      let listpokemon = document.createElement("li");
-      let button = document.createElement("button");
-      button.innerText = pokemon.name;
-      button.classList.add("button-class");
-      listpokemon.appendChild(button);
-      pokemonList.appendChild(listpokemon);
-    }
-  };
-})();
-
-console.log(pokemonRepository.getAll()); // []
-pokemonRepository.add({ name: 'Gardevoir', height: '1.2', types: ['GRASS', 'POISON'] });
-console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
-pokemonRepository.getAll().forEach(function(pokemon){ 
-  pokemonRepository.addListItem(pokemon);
-});
+    let pokemonList = [
+       {
+           name: 'Squirtle',
+           height: 1.1,
+           types: [
+               'WATER'
+           ]
+       },
+       {   
+           name: 'Pikachu',
+           height: 0.4,
+           types: [
+               'ELECTRIC'
+           ]
+   
+       },
+       {
+           name: 'Bulbasaur',
+           height: 0.5,
+           types: [
+               'GRASS', 
+               'POISON'
+           ]
+       } 
+       ];
+     return {
+       add: function(pokemon) {
+         pokemonList.push(pokemon);
+       },
+       getAll: function() {
+         return pokemonList;
+       },
+       showDetails: function(pokemon){
+        console.log(pokemon);
+       },
+       addListItem: function(pokemon) {
+         let pokemonList = document.querySelector(".pokemon-list");
+         let listpokemon = document.createElement("li");
+         let button = document.createElement("button");
+         button.addEventListener('click', function (event) {
+            console.log(event);
+          });
+         button.innerText = pokemon.name;
+         button.classList.add("button-class");
+         listpokemon.appendChild(button);
+         pokemonList.appendChild(listpokemon);
+       }
+     };
+   })();
+   
+   console.log(pokemonRepository.getAll()); // []
+   pokemonRepository.add({ name: 'Gardevoir', height: '1.2', types: ['GRASS', 'POISON'] });
+   console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
+   pokemonRepository.getAll().forEach(function(pokemon){ 
+     pokemonRepository.addListItem(pokemon);
+   });
