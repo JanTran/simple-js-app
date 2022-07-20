8b09275d3a6b9c059db847d701905437702a3278
Brendon Choi <choi.brendon2001@gmail.com>
2022-07-20 16:36:36 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,50 @@ let pokemonRepository = (function () {
            ]
        } 
        ];
+       function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      repository.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
+  }
+  function getAll() {
+    return repository;
+  }
+  function showDetails(pokemon){
+    console.log(pokemon);
+  }
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class");
+    listpokemon.appendChild(button);
+    pokemonList.appendChild(listpokemon);
+  }
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
+    showDetails: showDetails
+  };
+})();
+
+pokemonRepository.add({ name: 'Gardevoir', height: '1.2', types: ['GRASS', 'POISON'] });
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
+
+     /*
      return {
        add: function(pokemon) {
          pokemonList.push(pokemon);
@@ -55,3 +99,4 @@ let pokemonRepository = (function () {
    pokemonRepository.getAll().forEach(function(pokemon){ 
      pokemonRepository.addListItem(pokemon);
    });
+   */
