3044680c85239742dccfba468bf26127fe406be9
Brendon Choi <choi.brendon2001@gmail.com>
2022-07-22 10:18:33 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -57,7 +57,7 @@ let pokemonRepository = (function () {
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
     showDetails: showDetails
   };
 })();
