04ff3af66406a5f11379cf54b50dd53654f7e20a
Brendon Choi <choi.brendon2001@gmail.com>
2022-08-03 11:55:39 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -58,6 +58,52 @@ let pokemonRepository = (function () {
             console.error(e);
         })
     }
+    function showModal(pokemon) {
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = `Height: ${pokemon.height}`;
+
+    let imgElement = document.createElement('img');
+    imgElement.classList.add('img-element');
+    imgElement.src = pokemon.imageUrl;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(imgElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  }
 
     return {
         add: add,
